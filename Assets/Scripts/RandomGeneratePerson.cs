d44a893f5f167c43e994b33ec878f0347a3c4da9
KamilSzus <kamil-szustak1@wp.pl>
2021-12-13 20:06:17 +0100

    deleted polish sign and change project version (Kapka nie ruszaj :D)

--- Assets/Scripts/RandomGeneratePerson.cs
+++ Assets/Scripts/RandomGeneratePerson.cs
@@ -10,10 +10,10 @@ static public class RandomGeneratePerson
     private static readonly string[] nameMale = new string[] { "Ahmed", "Ahmman", "Alfred", "Ragnar", "Ivar", "Everyman", "Theoden", "Alrik", "Darik", "Gared", "Bezimienny", "Hagen", "Aragorn", "Joshep", "Kamil", "Harry", "Illidan", "Arthas", "Neek", "Geralt", "Vesemir", "Letho", "Lambert", "Eskel" };
     private static readonly string[] nameFemale = new string[] { "Inavera", "Lesha", "Selia", "Olive", "Lagertha", "Veila", "Arwena", "Wonda", "Diana", "Lena", "Sandra", "Sansa", "Arya", "Elune", "Kamila", "Tyrande", "Maiev", "Valera", "Erica", "Lova", "Yen", "Triss", "Monica", "Marcin" };
     private static readonly string[] gender = new string[] { "Male", "Famale" };
-    private static readonly string[] race = new string[] { "Ork", "Krasnolud", "Goblin", "Elf", "Cz³owiek", "Gnom", "Pó³-elf", "Pó³-ork", "Hobbit" };
-    private static readonly string[] lastName = new string[] { "Praudmoore", "von everec", "van Cleef", "Stormrage", "Kaji", "Majah", "Bloodchin", "Doomtrust", "Baggins", "Arevd", "Akkon", "Amit", "Mahale", "Hama", "Kernel", "Maraara", "Wedrowycz", "Pomidor", "Prostak", "Wiesniak", "Œwiniopas", "Bardak", "Bffadf" };
-    private static readonly string[] fraction = new string[] { "Bandyta", "Najemnik", "Kap³an", "Nikt", "Paladyn", "Stra¿nik", "Ch³op", "Wampir", "£owca Smoków", "Kupiec" };
-    private static readonly string[] faith = new string[] { "Kult S³oñca", "Kult Ognia", "Kult Œmierci", "Kult Powietrza", "Kult Wody", "Kult Ziemii" };
+    private static readonly string[] race = new string[] { "Ork", "Krasnolud", "Goblin", "Elf", "Cz³owiek", "Gnom", "Pol-elf", "Pol-ork", "Hobbit" };
+    private static readonly string[] lastName = new string[] { "Praudmoore", "von everec", "van Cleef", "Stormrage", "Kaji", "Majah", "Bloodchin", "Doomtrust", "Baggins", "Arevd", "Akkon", "Amit", "Mahale", "Hama", "Kernel", "Maraara", "Wedrowycz", "Pomidor", "Prostak", "Wiesniak", "Swiniopas", "Bardak" };
+    private static readonly string[] fraction = new string[] { "Bandyta", "Najemnik", "Kaplan", "Nikt", "Paladyn", "Straznik", "Chlop", "Wampir", "Lowca Smokow", "Kupiec" };
+    private static readonly string[] faith = new string[] { "Kult Slonca", "Kult Ognia", "Kult Smierci", "Kult Powietrza", "Kult Wody", "Kult Ziemii" };
 
     static public string generateRandomMaleName()
     {
