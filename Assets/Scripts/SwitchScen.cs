fd8c86efe374427b233365f6e8c27c42256edafb
KamilSzus <kamil-szustak1@wp.pl>
2022-01-21 12:34:06 +0100

    Add algorithm to find biggest value from score

--- Assets/Scripts/SwitchScen.cs
+++ Assets/Scripts/SwitchScen.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SwitchScen : MonoBehaviour
@@ -7,10 +8,46 @@ public class SwitchScen : MonoBehaviour
 
     public void loadScene()
     {
-        string scanName = scenLoader[Random.Range(0, scenLoader.Count)];
-        scenLoader.Remove(scanName);
-        SceneManager.LoadScene(scanName);
-    }
+        if (scenLoader.Count != 0)
+        {
+            string scanName = scenLoader[Random.Range(0, scenLoader.Count)];
+            scenLoader.Remove(scanName);
+            SceneManager.LoadScene(scanName);
+        }
+        else
+        {
+            var dict = new Dictionary<string, int>();
 
+            dict.Add("loyalty", ScoreVariable.loyalty);
+            dict.Add("morality", ScoreVariable.morality);
+            dict.Add("evil", ScoreVariable.evil);
+            dict.Add("greed", ScoreVariable.greed);
+
+
+            var keyOfMaxValue = dict.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
+
+            if (keyOfMaxValue == "loyalty")
+            {
+                //SceneManager.LoadScene("loyaltyEnd");
+                Debug.Log("loyaltyEnd");
+            }
+            else if (keyOfMaxValue == "morality")
+            {
+                //SceneManager.LoadScene("moralityEnd");
+                Debug.Log("moralityEnd");
+            }
+            else if (keyOfMaxValue == "evil")
+            {
+                //SceneManager.LoadScene("evilEnd");
+                Debug.Log("evilEnd");
+            }
+            else
+            {
+                //SceneManager.LoadScene("greedEnd");
+                Debug.Log("greedEnd");
+            }
+
+        }
+    }
     
 }
